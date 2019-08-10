1408408303b3a9e603e79ac97b72ddaa49623128
Qin <1044471292@qq.com>
2019-08-10 11:11:37 +0800

    完成展示电影票功能

--- src/main/resources/static/js/userBuy.js
+++ src/main/resources/static/js/userBuy.js
@@ -13,28 +13,29 @@ $(document).ready(function () {
     }
 
     function getSchedule(ticket,ticketListContent) {
-        console.log("getSchedule");
+        // console.log("getSchedule");
         getRequest(
             '/schedule/'+ticket.scheduleId,
             function (res) {
                 if(res.success){
                     var schedule = res.content;
-                    ticketListContent += '<div class="col-lg-10 table-wrapper"><div class="table"><div class="content"><div class="col-md-12 left">' +
+                    // console.log(schedule.startTime);
+                    // console.log(typeof (schedule.startTime));
+                    ticketListContent += '<div class="col-lg-5 table-wrapper"><div class="table"><div class="content"><div class="col-md-12 left">' +
                         '<div class="name"><b>' + schedule.movieName + '</b></div>' +
                         '<div class="hallNameAndSeat"><b>' +schedule.hallName +'  '
                         + (ticket.rowIndex+1)+ '排'+ (ticket.columnIndex+1) +'列</b></div><br>' +
-                        '<div class="startTime"><b>开始时间：' +
-                        schedule.startTime.toLocaleString() +
-                        '</b></div>' +
-                        '<div class="endTime"><b>结束时间：' +
-                        schedule.endTime.toLocaleString() +
+                        '<div class="startTime"><b>开始时间：<br/>' +
+                        schedule.startTime.slice(0,10)+' '+schedule.startTime.slice(11,16)+
+                        '</br></div>' +
+                        '<div class="endTime"><b>结束时间：<br/>' +
+                        schedule.endTime.slice(0,10)+' '+schedule.endTime.slice(11,16)+
                         '</b></div>' +
                         '<div class="price"><b>票价：￥' +
                         schedule.fare +
                         '</b></div>' +
-                        '<div class="payState"><b>' +
-                        (ticket.state==1?'支付成功':'支付未成功') +
-                        '</b></div>' +
+                        (ticket.state==1?'<div class="paySuccess">支付成功</div>':
+                            '<div class="payFail">支付未成功</div>') +
                         '</div></div></div></div>';
 
                     $('#ticket-list').append(ticketListContent);
